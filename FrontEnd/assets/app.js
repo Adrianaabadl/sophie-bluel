a2429159e26457ace0558a605670f6ed0054e134
Adrianaabadl <adri.abad.limon@gmail.com>
2025-08-08 14:54:44 +0200

    Initial test v1

--- FrontEnd/assets/app.js
+++ FrontEnd/assets/app.js
@@ -1,8 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
     const sendButton = document.getElementById('send-button');
+    const modal = document.getElementById('modal');
+    const closeBtn = document.querySelector('.close');
+    const gallery = document.getElementById("gallery");
 
+    // Lista de imágenes
+    const imageList = [
+        "abajour-tahina.png",
+        "appartement-paris-v.png",
+        "appartement-paris-x.png",
+        "appartement-paris-xviii.png",
+        "bar-lullaby-paris.png",
+        "hotel-first-arte-new-delhi.png",
+        "la-balisiere.png",
+        "le-coteau-cassis.png",
+        "restaurant-sushisen-londres.png",
+        "sophie-bluel.png",
+        "structures-thermopolis.png",
+        "villa-ferneze.png"
+    ];
+
+    // Insertar imágenes en el contenedor
+    imageList.forEach(fileName => {
+        const img = document.createElement("img");
+        img.src = `assets/images/${fileName}`; // ruta relativa
+        img.alt = fileName.split(".")[0];
+        gallery.appendChild(img);
+    });
+
+    // Abrir modal
     sendButton.addEventListener('click', function(event) {
         event.preventDefault();
-        console.log('Funny click');
+        modal.style.display = 'block';
+    });
+
+    // Cerrar modal con la X
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    // Cerrar modal clicando fuera del contenido
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
     });
 });
