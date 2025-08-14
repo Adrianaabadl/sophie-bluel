a58afd7191e74ce51ad1f6983bba165db28aa8bf
Adrianaabadl <adri.abad.limon@gmail.com>
2025-08-14 22:43:30 +0200

    new changes

--- FrontEnd/assets/app.js
+++ FrontEnd/assets/app.js
@@ -1,48 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
     const sendButton = document.getElementById('send-button');
-    const modal = document.getElementById('modal');
-    const closeBtn = document.querySelector('.close');
-    const gallery = document.getElementById("gallery");
+    const modalPhotoGallery = document.getElementById('modal-photogallery');
+    const closeBtns = document.querySelectorAll('.close');
+    const addPhotoButtom = document.getElementById("addPhoto");
+    const modalAddPhoto = document.getElementById('modal-addphoto');
 
-    // Lista de imágenes
-    const imageList = [
-        "abajour-tahina.png",
-        "appartement-paris-v.png",
-        "appartement-paris-x.png",
-        "appartement-paris-xviii.png",
-        "bar-lullaby-paris.png",
-        "hotel-first-arte-new-delhi.png",
-        "la-balisiere.png",
-        "le-coteau-cassis.png",
-        "restaurant-sushisen-londres.png",
-        "sophie-bluel.png",
-        "structures-thermopolis.png",
-        "villa-ferneze.png"
-    ];
-
-    // Insertar imágenes en el contenedor
-    imageList.forEach(fileName => {
-        const img = document.createElement("img");
-        img.src = `assets/images/${fileName}`; // ruta relativa
-        img.alt = fileName.split(".")[0];
-        gallery.appendChild(img);
-    });
-
-    // Abrir modal
+    // Open modal
     sendButton.addEventListener('click', function(event) {
         event.preventDefault();
-        modal.style.display = 'block';
+        modalPhotoGallery.style.display = 'block'
+
     });
 
-    // Cerrar modal con la X
-    closeBtn.addEventListener('click', () => {
-        modal.style.display = 'none';
+    // Close modal
+    closeBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modalPhotoGallery.style.display = 'none';
+        modalAddPhoto.style.display = 'none';
+        });
     });
 
-    // Cerrar modal clicando fuera del contenido
+    // Force close if user click outside
     window.addEventListener('click', (event) => {
         if (event.target === modal) {
-            modal.style.display = 'none';
+            modalPhotoGallery.style.display = 'none';
+            modalAddPhoto.style.display = 'none';
         }
     });
+
+    // Open second modal 
+     addPhotoButtom.addEventListener('click', function(event) {
+        event.preventDefault();
+        modalPhotoGallery.style.display = 'none'; // Close the other modal
+        modalAddPhoto.style.display = 'block' // Open the new modal
+
+    });
+
+
 });
