73f4e7c9cdb80f3cee3c7dd170769bdee58b9a8f
Adrianaabadl <adri.abad.limon@gmail.com>
2025-08-29 18:06:17 +0200

    add_login_page

--- Backend/routes/works.routes.js
+++ Backend/routes/works.routes.js
@@ -6,7 +6,7 @@ const checkWork = require('../middlewares/checkWork');
 const workCtrl = require('../controllers/works.controller');
 
 router.post('/', auth, multer, checkWork, workCtrl.create);
-router.get('/', workCtrl.findAll);
+router.get('/', workCtrl.findAll); // We can actually get all the projects without auth
 router.delete('/:id', auth, workCtrl.delete);
 
 module.exports = router;
